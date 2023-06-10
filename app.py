3656043ea3fd37a91acdd1e336f2d4c2a94b0b15
Victor Jung <123326026+victorkjung@users.noreply.github.com>
2023-06-10 09:48:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ st.title(':smile: Break-Even Analysis')
 st.title('How many :red[_units_] do you need to sell?')
 
 # Inputs
-units = int(st.text_input('Guess the Number of units', value='100').replace(',', ''))
-sell_price_per_unit = float(st.text_input('Selling price per unit', value='50.00').replace(',', ''))
-total_fixed_costs = float(st.text_input('Total fixed costs', value='1000.00').replace(',', ''))
-variable_costs_per_unit = float(st.text_input('Variable cost per unit', value='10.00').replace(',', ''))
+units = int(st.text_input('Guess the Number of units', value='0').replace(',', ''))
+sell_price_per_unit = float(st.text_input('Selling price per unit', value='0.00').replace(',', ''))
+total_fixed_costs = float(st.text_input('Total fixed costs', value='0.00').replace(',', ''))
+variable_costs_per_unit = float(st.text_input('Variable cost per unit', value='0.00').replace(',', ''))
 
 # Creating a progress bar
 latest_iteration = st.empty()
